efd9473a98915171502ac900c17b791fe1ae3781
satonga <spencer.tonga18@gmail.comgit config --global user.name satonga>
2021-11-03 13:36:16 -0600

    styles

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.use(express.static('public'))
+
 const port = process.env.PORT || 4005
 
 app.listen(port, () => console.log(`Listening on ${port}`))
