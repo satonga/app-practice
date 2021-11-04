a074a5a8fa2e56ecf08b1fecbd3b321270986fcc
satonga <spencer.tonga18@gmail.comgit config --global user.name satonga>
2021-11-04 13:32:34 -0600

    rollbar token

--- public/main.js
+++ public/main.js
@@ -1,1 +1,12 @@
 // Functions
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '078df177ed74422abaa982e73f6ebe85',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+const giveAlert = () => {
+    rollbar.info('User clicked on information link')
+    alert('Fill out the information on the following page for the puppy!')
+}
